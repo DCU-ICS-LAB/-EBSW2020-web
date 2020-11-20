aa41d2d715dc9d586933346a2fd73f8b3e07c337
sohyun1008 <wjsth26@naver.com>
2020-11-21 01:33:50 +0900

    modify route destination

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,10 @@
 
 export const API_BASE_URL = 'http://203.250.32.29:8080'
+
+export const DEFAULT_MAP_OPTION={
+    center: {
+        longitude: 127.0295333,
+        latitude: 37.49335
+    },
+    level: 10
+}
--- src/containers/RouteMapPage.js
+++ src/containers/RouteMapPage.js
@@ -6,7 +6,148 @@ import * as basicActions from 'store/modules/basic';
 import * as passengerActions from 'store/modules/passenger';
 import {
     RouteMap,
-    FooterBox
+    FooterBoximport React, { Component, Fragment } from 'react';
+    import { bindActionCreators } from 'redux';
+    import { withRouter } from 'react-router';
+    import { connect } from 'react-redux';
+    import * as basicActions from 'store/modules/basic';
+    import * as passengerActions from 'store/modules/passenger';
+    import {
+        RouteMap,
+        FooterBox
+      } from "components";
+      import {
+          DEFAULT_MAP_OPTION
+      } from 'constants/index'
+    import { FaDAndDBeyond } from 'react-icons/fa';
+    
+    const {kakao} = window;
+    /*global kakao*/
+    var map = null;
+    var markers = [];
+    var linePath=[];
+    class RouteMapPage extends Component {
+        _fbSelect = (idx) => {
+            const { basicActions } = this.props;
+            basicActions.fbSelect(idx);
+        }
+        _createBusRoute= (info,map,isTarget) =>{
+            
+            //var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+            
+            // var options = { //지도를 생성할 때 필요한 기본 옵션
+            //     center: new kakao.maps.LatLng(37.65245587442243, 126.77746762089282), //지도의 중심좌표.
+            //     level: 8 //지도의 레벨(확대, 축소 정도)
+            //     };
+            //     var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+                // 선을 구성하는 좌표 배열입니다. 이 좌표들을 이어서 선을 표시합니다
+                info.map((item,idx)=>{
+                    const longitude = item.get('x');
+                    const latitude = item.get('y')
+    
+                    linePath.push(
+                        this._createMark({
+                            longitude: longitude,
+                            latitude: latitude,
+                            idx: idx,
+                            map: map,
+                            isTarget: isTarget
+                        })
+                    )
+                })
+                this._createLine({
+                    linePath: linePath,
+                    map: map,
+                    isTarget: isTarget
+                })
+        }
+        _createMark = ({latitude,longitude, idx, map})=>{
+    
+            const pos = new kakao.maps.LatLng(latitude, longitude)
+    
+            var marker = new kakao.maps.Marker({
+                position: pos, // 마커의 위치
+            })
+    
+            marker.setMap(map);
+             kakao.maps.event.addListener(marker, 'click', function() {
+                
+            });
+            marker.setMap(map);
+            
+            markers.push(marker) 
+            
+            return pos
+    } 
+        _createLine = ({linePath,map}) => {
+            if(linePath.size<=1) return
+    
+            var polyline = new kakao.maps.Polyline({
+                path: linePath, // 선을 구성하는 좌표배열 입니다
+                strokeWeight: 5, // 선의 두께 입니다
+                strokeColor: '#FF0000', // 선의 색깔입니다
+                strokeOpacity: 0.7, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+                strokeStyle: 'solid' // 선의 스타일입니다
+            });
+    
+                polyline.setMap(map);
+        }
+        
+        componentDidMount() {
+            const { passengerActions} = this.props;
+            const routeId =200000279;
+            passengerActions.getBusRouteStationList(routeId);
+            kakao.maps.load(() => {
+                const el = document.getElementById('map')
+                const mapOption = {
+                    center: new kakao.maps.LatLng(DEFAULT_MAP_OPTION.center.latitude, DEFAULT_MAP_OPTION.center.longitude),
+                    level: DEFAULT_MAP_OPTION.level
+                }
+                map = new kakao.maps.Map(el, mapOption)
+            })
+        }
+        
+        componentDidUpdate() {
+            const { busRouteStationList} = this.props;
+            this._createBusRoute(busRouteStationList, map, true) 
+            
+        }
+        render() {
+            const {
+                select,
+                busRouteStationList,
+                firstName
+            } = this.props;
+            return (
+                
+               
+                <Fragment>
+                    <RouteMap busRouteStationList={busRouteStationList}
+                    
+                    firstName={busRouteStationList.getIn([linePath.length-1,'stationName'])}
+                    // map={this._createBusRoute}
+                    />
+                    <FooterBox select={select} fbSelect={this._fbSelect}/>
+                </Fragment>
+            )
+        }
+    }
+    
+    
+    export default withRouter(
+        connect(
+            // props 로 넣어줄 스토어 상태값
+            state => ({
+                select: state.basic.getIn(['basic', 'select']),
+                busRouteStationList: state.passenger.get('busRouteStationList'),
+            }),
+            // props 로 넣어줄 액션 생성함수
+            dispatch => ({
+                basicActions: bindActionCreators(basicActions, dispatch),
+                passengerActions: bindActionCreators(passengerActions, dispatch)
+            })
+        )(RouteMapPage)
+    )
   } from "components";
 import { FaDAndDBeyond } from 'react-icons/fa';
 
